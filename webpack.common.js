1d6b097a0d042c813d20525e394296ede1a528ee
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-10-25 11:56:23 -0600

    Add the html core structure and styles

--- webpack.common.js
+++ webpack.common.js
@@ -5,11 +5,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
    entry: './src/index.js',
    plugins: [
      new HtmlWebpackPlugin({
-       title: 'Production',
+       template: "./src/template.html",
      }),
     ],
     output: {
-     filename: '[name].bundle.js',
+     filename: 'main.js',
      path: path.resolve(__dirname, 'dist'),
      clean: true,
     },
