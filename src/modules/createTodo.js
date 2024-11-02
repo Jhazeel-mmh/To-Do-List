6c3fedab86053bf00726c7186183bdd7b9bef8bb
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-11-02 00:14:17 -0600

    Fix the default parameters and imput managment: the function no longer set this.date as undefined

--- src/modules/createTodo.js
+++ src/modules/createTodo.js
@@ -1,5 +1,5 @@
 import { Todo } from "./todo";
 
 export function createTodo(task, done = false, notes = "", priority = null, date = new Date(), category = "", id = null){
-    return new Todo(task, done, notes, priority, date, category, id);
+    return new Todo(task, done, notes, priority, new Date(date), category, id);
 }
