cc2eff43dc69d8d1ed7acf1e9de23f2938f155f6
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-11-02 20:59:43 -0600

    Add a checkbox, a remove btns on the todo form and fix the display of the checkbox of the todos

--- src/modules/DOMController.js
+++ src/modules/DOMController.js
@@ -8,20 +8,21 @@ class DOMController {
     updateCategorys(list){
         this.categorys = list;
     }
-    // todo: addthe add function to display a form that adds a div. Remember to get the categorys from all the task and display them in a select input
-    // probably you gotta add some other function in the storageController 
+    // TODO: addthe add function to display a form that adds a div. Remember to get the categorys from all the task and display them in a select input,  probably you gotta add some other function in the storageController 
     // DONE, I UPDATE THE IMPLEMENTATION OF displayFormOfTodo() TO USE IN CASE OF ADD/SHOW A TODO 
 
 
     // ADD A ID TO THE TASK DIV TO IMPROVE THE JS DELEGATION EVENT WHEN SOMEONE CLICKS  IN THE TODO TO SHOW HIS INFO
+
     showTasks(list){
         let mainDiv = $(".main");
         list.forEach(todo => {
             let task = c$("div", "task-item")
             task.innerHTML = `
-                <input type="checkbox" id="${todo.id}" class="todo-checkbox">
+                <input type="checkbox" id="${todo.id}" class="todo-checkbox" ${todo.done ? "checked" : ""}>
                 <p class="todo-item-todo">${todo.task}</p>
             `
+            task.id = "todo-id-" + todo.id;
             mainDiv.appendChild(task);
         });
     }
@@ -44,10 +45,13 @@ class DOMController {
         } else {
             prioritySelectedElement = this.determineSelectedByPriority()
         }
+
+        // TODO add some btns to remove  a todo
        
         div.innerHTML = `
             <form id="add-task-form">
                 <button class="close-details-btn">X</button>
+                <button class="remove-todo-btn">Remove</button>
                 <div class="todo-inputs">
                     <p id="todo-wrapper">
                         <input id="todo-task" value="${todo.task ? todo.task : ""}">
@@ -56,8 +60,8 @@ class DOMController {
                         <textarea id="todo-notes">${todo.notes ? todo.notes : ""}</textarea>    
                     </p>
                     <p id="priority-wrapper">
-                        <label for="taskPriority">Priority:</label>
-                        <select id="taskPriority" name="priority">
+                        <label for="todo-priority">Priority:</label>
+                        <select id="todo-priority" name="priority">
                             ${prioritySelectedElement}
                         </select>
                     </p>
@@ -69,6 +73,10 @@ class DOMController {
                         <label for="todo-category">Category: </label>
                         ${this.displayCategoryInput(todo.category ? todo.category : "")}
                         ${this.displayCategoryDatalist(this.categorys)}
+                    </p>
+                    <p id="check-wrapper">
+                        <label for="todo-check">Done:</label>
+                        <input type="checkbox" ${todo.done ? "checked" : ""} id="todo-check>
                     </p>
                     <p id="submit-wrapper">
                         <input type="submit" value="Save" id="submitform">
